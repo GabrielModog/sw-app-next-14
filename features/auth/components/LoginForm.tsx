4c725e916f5b005ec8db61f53329c7a9afb9adfa
GabrielModog <modog.dev@gmail.com>
2024-07-22 04:00:33 -0300

    [feat] Add login form component

---
+++ features/auth/components/LoginForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6, "Your password most be at least 6 characters."),
+});
+
+const defaultValues = {
+  email: "",
+  password: "",
+};
+
+interface LoginFormProps {
+  onSubmit: (values: z.infer<typeof formSchema>) => void
+}
+
+export default function LoginForm(props: LoginFormProps) {
+  const {onSubmit} = props
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+
+  function onSubmitHandler(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    onSubmit(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmitHandler)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Login</Button>
+      </form>
+    </Form>
+  );
+}
