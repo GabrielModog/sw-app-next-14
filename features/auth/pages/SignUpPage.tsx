5043bba7d140a0c14614a24681659f1731d89624
GabrielModog <modog.dev@gmail.com>
2024-07-23 01:35:37 -0300

    [feat] add login and signup page

--- features/auth/pages/SignUpPage.tsx
+++ features/auth/pages/SignUpPage.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import LoginForm from "../components/LoginForm";
+import SignUpForm from "../components/SignUpForm";
 
-export default function LoginPage() {
+export default function SignUpPage() {
   return <section className="w-full">
     <h2 className="text-2xl font-semibold mb-4 pb-4">
-      Login
+      Sign Up
     </h2>
-    <LoginForm />
+    <SignUpForm />
   </section>
 }
